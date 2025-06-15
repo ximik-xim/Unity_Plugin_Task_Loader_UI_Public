e9aaf29f3009f2c7593b469cd92692f7be030313
NO-243PCBER03I9\ENP <ximik-xim@mail.ru>
2025-06-15 12:41:46 +0300

    - Добавление поддержки TList(0.6.0) - Добавление бэкапов

--- Task Loader UI V2/Logger/Key Task Loader Type Log/TList/IdentifierAndData_KeyTaskLoaderTypeLog.cs
+++ Task Loader UI V2/Logger/Key Task Loader Type Log/TList/IdentifierAndData_KeyTaskLoaderTypeLog.cs
@@ -12,4 +12,16 @@ public class IdentifierAndData_KeyTaskLoaderTypeLog : AbsIdentifierAndData<Indif
  {
   return _dataKey;
  }
+ 
+#if UNITY_EDITOR
+ public override string GetJsonSaveData()
+ {
+  return JsonUtility.ToJson(_dataKey);
+ }
+
+ public override void SetJsonData(string json)
+ {
+  _dataKey = JsonUtility.FromJson<KeyTaskLoaderTypeLog>(json);
+ }
+#endif
 }
